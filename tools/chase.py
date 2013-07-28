02ec65e7cc29f85e2fcc379e6044775f2b791f00
Micah Elizabeth Scott <micah@scanlime.org>
2013-07-28 09:50:38 -0700

    Another simple tool: Light LEDs in order

---
+++ tools/chase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Light each LED in sequence, and repeat.
+
+import opc, time
+
+numLEDs = 512
+client = opc.Client('localhost:7890')
+
+while True:
+	for i in range(numLEDs):
+		pixels = [ (0,0,0) ] * numLEDs
+		pixels[i] = (255, 255, 255)
+		client.put_pixels(pixels)
+		time.sleep(0.1)
