33c4c5a43560cecd877b8441099a23205add0d3b
Micah Elizabeth Scott <micah@scanlime.org>
2013-10-07 23:57:35 -0700

    Disable IRQs inside watchdog_refresh()
    Otherwise, some call sites will certainly forget this...

--- firmware/mk20dx128.h
+++ firmware/mk20dx128.h
@@ -1591,8 +1591,10 @@ extern void software_isr(void);
 
 static inline void watchdog_refresh(void)
 {
+	__disable_irq();
 	WDOG_REFRESH = 0xA602;
 	WDOG_REFRESH = 0xB480;
+	__enable_irq();
 }
 
 static inline void watchdog_reboot(void)
