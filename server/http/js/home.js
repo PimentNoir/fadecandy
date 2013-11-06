7834221495a29ffd76e3c54fb69461d50fce1ea8
Micah Elizabeth Scott <micah@scanlime.org>
2013-11-05 21:02:48 -0800

    Tidy up some JavaScript and CSS

--- server/http/js/home.js
+++ server/http/js/home.js
@@ -2,121 +2,161 @@
  * Fadecandy Server web UI
  */
 
+jQuery(function ($) {
+    'use strict';
 
-/*
- * Utility to make sensibly pretty-printed and hilighted JSON.
- * Small values are kept inline, larger arrays and objects are broken out onto multiple lines.
- */
+    var Utils = {
 
-function SensibleJsonToHtml(obj)
-{
-    var escape = function(str) {
-        return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    }
+        sensibleJsonToHtml: function(obj) {
+            /*
+             * Utility to make sensibly pretty-printed and hilighted JSON.
+             * Small values are kept inline, larger arrays and objects are broken out onto multiple lines.
+             */
 
-    var visit = function(obj, indentLevel)
-    {
-        var objString = JSON.stringify(obj);
-        var oneLiner = objString.length < 40;
-        var items = [];
-        var indent = '    ';
-        var separator = oneLiner ? ' ' : '\n';
-        indentLevel = oneLiner ? '' : (indentLevel || '');
-        var nextIndent = oneLiner ? '' : (indentLevel + indent);
+            var escape = function(str) {
+                return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+            }
 
-        // Simple value?
-        if (obj == null || obj == undefined || typeof(obj) != "object") {
-            return '<span class="json-' + typeof(obj) + '">' + escape(objString) + '</span>';
+            var visit = function(obj, indentLevel) {
+                var objString = JSON.stringify(obj);
+                var oneLiner = objString.length < 40;
+                var items = [];
+                var indent = '    ';
+                var separator = oneLiner ? ' ' : '\n';
+                indentLevel = oneLiner ? '' : (indentLevel || '');
+                var nextIndent = oneLiner ? '' : (indentLevel + indent);
+
+                // Simple value?
+                if (obj == null || obj == undefined || typeof(obj) != "object") {
+                    return '<span class="json-' + typeof(obj) + '">' + escape(objString) + '</span>';
+                }
+
+                if ($.isArray(obj)) {
+                    // Array object
+                    for (var i = 0; i < obj.length; i++) {
+                        items.push(nextIndent + visit(obj[i], nextIndent));
+                    }
+                    return '<span class="json-punctuation">[</span>'
+                        + separator + items.join(',' + separator)
+                        + separator + indentLevel + '<span class="json-punctuation">]</span>';
+
+                } else {
+                    // Dictionary object
+                    for (var k in obj) {
+                        items.push(
+                            nextIndent + '<span class="json-key">' + escape(JSON.stringify(k)) + '</span>'
+                            + '<span class="json-punctuation">:</span> '
+                            + visit(obj[k], nextIndent)
+                        );
+                    }
+                    return '<span class="json-punctuation">{</span>'
+                        + separator + items.join(',' + separator)
+                        + separator + indentLevel + '<span class="json-punctuation">}</span>';
+                }
+            }
+
+            return visit(obj);
         }
 
-        if ($.isArray(obj)) {
-            // Array object
-            for (var i = 0; i < obj.length; i++) {
-                items.push(nextIndent + visit(obj[i], nextIndent));
-            }
-            return '<span class="json-punctuation">[</span>'
-                + separator + items.join(',' + separator)
-                + separator + indentLevel + '<span class="json-punctuation">]</span>';
+    };
 
-        } else {
-            // Dictionary object
-            for (var k in obj) {
-                items.push(
-                    nextIndent + '<span class="json-key">' + escape(JSON.stringify(k)) + '</span>'
-                    + '<span class="json-punctuation">:</span> '
-                    + visit(obj[k], nextIndent)
-                );
-            }
-            return '<span class="json-punctuation">{</span>'
-                + separator + items.join(',' + separator)
-                + separator + indentLevel + '<span class="json-punctuation">}</span>';
-        }
-    }
+    var ConnectionManager = {
 
-    return visit(obj);
-}
+        init: function() {
+            // Compatibility check
+            if (!this.isBrowserSupported())
+                return;
+            $("#browser-not-supported").addClass("hide");
 
+            // Make the initial connection attempt. We can reconnect manually if the connection is lost
+            $(".connect-button").on("click", this.connect);
+            this.connect();
+        },
 
-$(document).ready(function()
-{
-    // Do we support the browser? Right now we only care about checking for WebSockets.
-    if (window.WebSocket) {
-        $("#browser-not-supported").addClass("hide");
-    } else {
-        return;
-    }
+        isBrowserSupported: function() {
+            // Currently we only care about WebSockets
+            return window.WebSocket;
+        },
 
-    var ws;
-    var serverURL = window.location.origin.replace("http://", "ws://");
-    $("#server-url").text(serverURL);
+        connect: function() {
+            /*
+             * (Re)connect to the server. This manages our WebSocket's life cycle, and 
+             * updates the UI according to our current connection state.
+             */
 
-    /*
-     * (Re)connect to the server. This creates our WebSocket, and holds its callbacks.
-     */
+            this.serverURL = window.location.origin.replace("http://", "ws://");
+            this.ws = new WebSocket(this.serverURL);
+            this.ws.onerror = this.onError;
+            this.ws.onclose = this.onClose;
+            this.ws.onopen = this.onOpen;
+            this.ws.onmessage = this.onMessage;
 
-    function Connect()
-    {
-        $("#connection-error,#connection-closed,#connection-complete").addClass("hide");
-        $("#connection-in-progress").removeClass("hide");
+            $("#server-url").text(this.serverURL);
+            $("#connection-error,#connection-closed,#connection-complete").addClass("hide");
+            $("#connection-in-progress").removeClass("hide");
+        },
 
-        ws = new WebSocket(serverURL);
+        sendJson: function(message) {
+            /*
+             * Send a JSON message to fcserver. Note that it's also possible to send binary
+             * messages with raw OPC packets, and this is the preferred way to do high-performance
+             * animation.
+             */
 
-        ws.onerror = function(evt)
-        {
+            this.ws.send(JSON.stringify(message));
+        },
+
+        onError: function() {
             $("#connection-error").removeClass("hide");
             $("#connection-complete,#connection-in-progress").addClass("hide");
-        };
+        },
 
-        ws.onclose = function(evt)
-        {
+        onClose: function() {
             $("#connection-closed").removeClass("hide");
             $("#connection-complete,#connection-in-progress").addClass("hide");
-        };
+        },
 
-        ws.onopen = function(evt)
-        {
+        onOpen: function() {
             $("#connection-complete").removeClass("hide");
             $("#connection-in-progress").addClass("hide");
 
             // Fire off some requests to ask about the current server state
-            ws.send(JSON.stringify({ type: "server_info" }));
-            ws.send(JSON.stringify({ type: "list_connected_devices" }));
-        };
+            ConnectionManager.sendJson({ type: "server_info" });
+            ConnectionManager.sendJson({ type: "list_connected_devices" });
+        },
 
-        ws.onmessage = function(evt)
-        {
+        onMessage: function(evt) {
             var msg = JSON.parse(evt.data);
-
-            console.log(msg);
 
             if (msg.type == "server_info") {
                 $("#server-version").text(msg.version);
-                $("#server-config").html(SensibleJsonToHtml(msg.config));
+                $("#server-config").html(Utils.sensibleJsonToHtml(msg.config));
+                return;
             }
-        };
+
+            if (msg.type == "list_connected_devices" ||         // Initial connection list
+                msg.type == "connected_devices_changed") {      // Hotplug event 
+                ConnectionManager.updateDeviceList(msg.devices);
+                return;
+            }
+        },
+
+        updateDeviceList: function(devices) {
+            /*
+             * We received a new list of devices, either after our connection completed
+             * or after a hotplug event. Walk the list of devices and update our model.
+             */
+
+            // Are there any devices at all?
+            if (devices.length == 0) {
+                $("#no-devices-connected").removeClass("hide");
+                $("#devices-table").addClass("hide");
+            } else {
+                $("#no-devices-connected").addClass("hide");
+                $("#devices-table").removeClass("hide");
+            }
+        }
     }
 
-    // Make the initial connection attempt. We can reconnect manually if the connection is lost
-    $(".connect-button").click(Connect);
-    Connect();
+    ConnectionManager.init();
 });
