b7f6868d93e9598220b4bf2583a8f59d102e500a
Micah Elizabeth Scott <micah@scanlime.org>
2013-11-05 19:59:04 -0800

    Cute little JSON pretty-printer

--- server/http/js/home.js
+++ server/http/js/home.js
@@ -2,8 +2,63 @@
  * Fadecandy Server web UI
  */
 
-$(document).ready(function(){
 
+/*
+ * Utility to make sensibly pretty-printed and hilighted JSON.
+ * Small values are kept inline, larger arrays and objects are broken out onto multiple lines.
+ */
+
+function SensibleJsonToHtml(obj)
+{
+    var escape = function(str) {
+        return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    }
+
+    var visit = function(obj, indentLevel)
+    {
+        var objString = JSON.stringify(obj);
+        var oneLiner = objString.length < 40;
+        var items = [];
+        var indent = '    ';
+        var separator = oneLiner ? ' ' : '\n';
+        indentLevel = oneLiner ? '' : (indentLevel || '');
+        var nextIndent = oneLiner ? '' : (indentLevel + indent);
+
+        // Simple value?
+        if (obj == null || obj == undefined || typeof(obj) != "object") {
+            return '<span class="json-' + typeof(obj) + '">' + escape(objString) + '</span>';
+        }
+
+        if ($.isArray(obj)) {
+            // Array object
+            for (var i = 0; i < obj.length; i++) {
+                items.push(nextIndent + visit(obj[i], nextIndent));
+            }
+            return '<span class="json-punctuation">[</span>'
+                + separator + items.join(',' + separator)
+                + separator + indentLevel + '<span class="json-punctuation">]</span>';
+
+        } else {
+            // Dictionary object
+            for (var k in obj) {
+                items.push(
+                    nextIndent + '<span class="json-key">' + escape(JSON.stringify(k)) + '</span>'
+                    + '<span class="json-punctuation">:</span> '
+                    + visit(obj[k], nextIndent)
+                );
+            }
+            return '<span class="json-punctuation">{</span>'
+                + separator + items.join(',' + separator)
+                + separator + indentLevel + '<span class="json-punctuation">}</span>';
+        }
+    }
+
+    return visit(obj);
+}
+
+
+$(document).ready(function()
+{
     // Do we support the browser? Right now we only care about checking for WebSockets.
     if (window.WebSocket) {
         $("#browser-not-supported").addClass("hide");
@@ -15,24 +70,31 @@ $(document).ready(function(){
     var serverURL = window.location.origin.replace("http://", "ws://");
     $("#server-url").text(serverURL);
 
-    // (Re)connect to the server
-    var connect = function() {
+    /*
+     * (Re)connect to the server. This creates our WebSocket, and holds its callbacks.
+     */
+
+    function Connect()
+    {
         $("#connection-error,#connection-closed,#connection-complete").addClass("hide");
         $("#connection-in-progress").removeClass("hide");
 
         ws = new WebSocket(serverURL);
 
-        ws.onerror = function(evt) {
+        ws.onerror = function(evt)
+        {
             $("#connection-error").removeClass("hide");
             $("#connection-complete,#connection-in-progress").addClass("hide");
         };
 
-        ws.onclose = function(evt) {
+        ws.onclose = function(evt)
+        {
             $("#connection-closed").removeClass("hide");
             $("#connection-complete,#connection-in-progress").addClass("hide");
         };
 
-        ws.onopen = function(evt) {
+        ws.onopen = function(evt)
+        {
             $("#connection-complete").removeClass("hide");
             $("#connection-in-progress").addClass("hide");
 
@@ -41,19 +103,20 @@ $(document).ready(function(){
             ws.send(JSON.stringify({ type: "list_connected_devices" }));
         };
 
-        ws.onmessage = function(evt) {
+        ws.onmessage = function(evt)
+        {
             var msg = JSON.parse(evt.data);
+
+            console.log(msg);
 
             if (msg.type == "server_info") {
                 $("#server-version").text(msg.version);
-                $("#server-config").text(JSON.stringify(msg.config, undefined, 2));
+                $("#server-config").html(SensibleJsonToHtml(msg.config));
             }
         };
-    };
+    }
 
-    // We can reconnect manually if the connection is lost
-    $(".connect-button").click(connect);
-
-    // Make the initial connection attempt
-    connect();
+    // Make the initial connection attempt. We can reconnect manually if the connection is lost
+    $(".connect-button").click(Connect);
+    Connect();
 });
