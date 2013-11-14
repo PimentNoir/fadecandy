ae4dbefbda15527a03e27b91d7bb45fba1d520c7
Micah Elizabeth Scott <beth@scanlime.org>
2013-11-14 05:52:34 +0000

    Linux build fixes

---
+++ server/src/config.h
@@ -0,0 +1,7 @@
+#ifndef TIMESPEC_TO_TIMEVAL
+#define TIMESPEC_TO_TIMEVAL(tv, ts)                                     \
+        do {                                                            \
+                (tv)->tv_sec = (ts)->tv_sec;                            \
+                (tv)->tv_usec = (ts)->tv_nsec / 1000;                   \
+        } while (0)
+#endif
