84853006bb94f6c5177ad35c3cc6e385b00ef381
Micah Elizabeth Scott <micah@scanlime.org>
2014-02-26 14:07:24 -0800

    Handle negative hue values in hsv()

--- examples/node/opc.js
+++ examples/node/opc.js
@@ -169,17 +169,17 @@ OPC.hsv = function(h, s, v)
      * 2013-08-10, Brian Grinstead, MIT License
      */
 
-    h *= 6;
+    h = (h % 1) * 6;
+    if (h < 0) h += 6;
 
     var i = h | 0,
         f = h - i,
         p = v * (1 - s),
         q = v * (1 - f * s),
         t = v * (1 - (1 - f) * s),
-        mod = i % 6,
-        r = [v, q, p, p, t, v][mod],
-        g = [t, v, v, q, p, p][mod],
-        b = [p, p, t, v, v, q][mod];
+        r = [v, q, p, p, t, v][i],
+        g = [t, v, v, q, p, p][i],
+        b = [p, p, t, v, v, q][i];
 
     return [ r * 255, g * 255, b * 255 ];
 }
